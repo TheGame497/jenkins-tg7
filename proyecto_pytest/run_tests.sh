847544b9bc458f645aeb4b4f7da2c367fdffcf07
Andres M <tg7ishere@gmail.com>
2026-05-02 21:24:28 -0600

    version corregida

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
-echo "activando el entorno virtual"
-if [ ! "-d venv" ]; then
+set -e
+
+echo "Activando el entorno virtual"
+
+# crear venv si no existe
+if [ ! -d venv ]; then
   python3 -m venv venv
 fi
 
+# activar venv (Linux/Jenkins)
 if [ -f "venv/bin/activate" ]; then
-  source venv/bin/activate
-elif [ -f "venv/Scripts/activate" ]; then
-  source venv/bin/activate
+  . venv/bin/activate
 else
-    echo "Error no se activo el env"
-    exit 1
+  echo "Error: no se pudo activar el entorno virtual"
+  exit 1
 fi
 
+echo "verificando entorno"
+which python
+which pip
+
 echo "instalando dependencias"
-pip install --upgrade pip --break-system-packages
-pip install -r requirements.txt --break-system-packages
+pip install --upgrade pip
+pip install -r requirements.txt
 
 echo "ejecutando pruebas con pytest"
-pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
+mkdir -p reports
+pytest tests/ \
+  --junitxml=reports/test-results.xml \
+  --html=reports/test-results.html \
+  --self-contained-html
+
 echo "pruebas finalizadas resultados en reports"
