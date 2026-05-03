f4a910af4001d7e199965fe5fedc8bbbbc86a826
Andres M <tg7ishere@gmail.com>
2026-05-02 21:14:00 -0600

    version corregida

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -3,11 +3,19 @@ echo "activando el entorno virtual"
 if [ ! "-d venv" ]; then
   python3 -m venv venv
 fi
-source venv/bin/activate
+
+if [ ! "venv/bin/activate" ]; then
+  source venv/bin/activate
+elif [ -f "venv/Scripts/activate" ]
+  source venv/bin/activate
+else
+    echo"Error no se activo el env"
+    exit 1
+fi
 
 echo "instalando dependencias"
-pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade pip --break-system-packages
+pip install -r requirements.txt --break-system-packages
 
 echo "ejecutando pruebas con pytest"
 pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
